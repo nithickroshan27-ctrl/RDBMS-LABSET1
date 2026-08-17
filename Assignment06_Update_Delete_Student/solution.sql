9ee2b7194378def416097c07d1d8280b44496696
nithickroshan27-ctrl <nithickroshan27@gmail.com>
2026-08-17 12:45:14 +0530

    Revise SQL script for student table operations
    Updated SQL script to create a new student table and perform updates and deletions.

--- Assignment06_Update_Delete_Student/solution.sql
+++ Assignment06_Update_Delete_Student/solution.sql
@@ -1,25 +1,29 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20),
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5),
-    Email VARCHAR(30),
-    PhoneNumber BIGINT
-);
-
-INSERT INTO Student(StudentID,StudentName,Gender,DepartmentID)
-VALUES
-(1001,'Arun','Male',101),
-(1002,'Divya','Female',102),
-(1003,'Karthik','Male',101);
 
 -- Update Karthik's DepartmentID
 
 -- Delete StudentID 1002
 
 -- Display all records
+
+CREATE TABLE student (
+    studentID INT PRIMARY KEY,
+    studentName VARCHAR(50),
+    Department_ID INT
+);
+INSERT INTO STUDENT VALUES (1001,'ARUN',101);
+INSERT INTO STUDENT VALUES (1002,'KUMAR',102);
+INSERT INTO STUDENT VALUES (1003,'KARTHIK',101);
+INSERT INTO STUDENT VALUES (1004,'RAVI',104);  
+    
+update student
+
+set Department_ID=103
+
+where studet+name ='karthi';
+
+
+Delete FROM student
+where student_ID =1002;
+
+Select* FROM student;
+
